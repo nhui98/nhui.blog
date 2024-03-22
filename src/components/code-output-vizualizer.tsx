9d6098060ef5503647e7928c1b1347a0470c5552
Nathan Hui <hui.n1998@gmail.com>
2024-03-22 11:12:30 +0000

    feat: code output visualizer

---
+++ src/components/code-output-vizualizer.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
+import { useMediaQuery } from "@/hooks/use-media-query";
+
+export function CodeOutputVizualizer({
+  children,
+  resizable,
+}: {
+  children: React.ReactNode;
+  resizable?: boolean;
+}) {
+  const isMedium = useMediaQuery("(min-width: 768px)");
+
+  const size = !isMedium || !resizable ? 100 : 95;
+
+  return (
+    <div>
+      <ResizablePanelGroup direction="horizontal">
+        <ResizablePanel
+          defaultSize={size}
+          maxSize={size}
+          className="md:min-w-[350px]"
+        >
+          {children}
+        </ResizablePanel>
+        {resizable && (
+          <ResizableHandle className="hidden translate-x-4 bg-transparent md:flex dark:bg-transparent">
+            <div className="z-10 flex h-8 w-2 items-center justify-center rounded-full bg-gray-700">
+              <div className="size-2.5" />
+            </div>
+          </ResizableHandle>
+        )}
+
+        <ResizablePanel defaultSize={100 - size} minSize={100 - size} />
+      </ResizablePanelGroup>
+    </div>
+  );
+}
