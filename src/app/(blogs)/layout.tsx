71db51b0fd2047a33c047c42a4ff994c8d600fbf
Nathan Hui <hui.n1998@gmail.com>
2024-03-24 21:34:57 +0000

    fix: overflow x on small screen

--- src/app/(blogs)/layout.tsx
+++ src/app/(blogs)/layout.tsx
@@ -5,7 +5,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <Container
       className={cn(
-        "prose prose-gray max-w-[736px] dark:prose-invert",
+        "prose prose-gray max-w-[736px] overflow-x-hidden dark:prose-invert",
         "prose-a:text-blue-500 dark:prose-a:text-purple-500",
       )}
     >
